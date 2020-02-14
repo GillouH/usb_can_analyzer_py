d7266c266b0e15e5d503546817a0ba7f6a734ae9
Gillou <gilles.hubert@isen.yncrea.fr>
2020-02-14 16:19:14 +0100

    First commit.

---
+++ usb_can_analyzer/unittests/test_usb_can_analyzer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+from unittest import TestCase, main
+
+from usb_can_analyzer import Converter
+
+
+class Converter_Test(TestCase):
+	pass
+
+
+if __name__ == "__main__":
+	try:
+		unittest.main()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		pass
